d41d3a63c389362eca702bf6d1dd876378c696fa
Ryan Zaki <rzaki@c20g.com>
2019-01-24 00:45:27 -0500

    made the card more readable with some string replacements

--- index.js
+++ index.js
@@ -47,6 +47,11 @@ HowTo.prototype.eventHandlers.onLaunch = (launchRequest, session, response) => {
   const repromptText = 'For instructions on what you can say, please say help me.';
   response.ask(speechText, repromptText);
 };
+const speechToCard = speechStr => speechStr
+  .replace(/ sharp/g, '#')
+  .replace(/ay/g, 'a')
+  .toUpperCase()
+  .replace(/ FLAT/g, 'b');
 
 HowTo.prototype.intentHandlers = {
   GetScale: (intent, session, response) => {
@@ -70,7 +75,7 @@ HowTo.prototype.intentHandlers = {
         speech: `The notes are ${scale}`,
         type: AlexaSkill.speechOutputType.PLAIN_TEXT,
       };
-      response.tellWithCard(speechOutput, cardTitle, scale);
+      response.tellWithCard(speechOutput, cardTitle, speechToCard(scale));
     } else {
       let speech = '';
       if (scaleName && patternName) {
@@ -117,125 +122,4 @@ HowTo.prototype.intentHandlers = {
 exports.handler = (event, context) => {
   const howTo = new HowTo();
   howTo.execute(event, context);
-};
-
-
-
-/**
-    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-
-        http://aws.amazon.com/apache2.0/
-
-    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-*/
-
-/**
- * This sample shows how to create a Lambda function for handling Alexa Skill requests that:
- *
- * - Custom slot type: demonstrates using custom slot types to handle a finite set of known values
- *
- * Examples:
- * One-shot model:
- *  User: "Alexa, ask Scale Helper notes in A Major."
- *  Alexa: "(reads back notes in the A major scale)"
- */
-
-'use strict';
-
-var AlexaSkill = require('./AlexaSkill'),
-    scales = require('./scales');
-
-var APP_ID = "amzn1.ask.skill.3dadf9a2-ce7e-4ca5-a8ca-5ab7809a3967";
-
-
-var HowTo = function () {
-    AlexaSkill.call(this, APP_ID);
-};
-
-// Extend AlexaSkill
-HowTo.prototype = Object.create(AlexaSkill.prototype);
-HowTo.prototype.constructor = HowTo;
-
-HowTo.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    var speechText = "Welcome to Scale Helper. Just name a scale, such as G Melodic Minor. Now, which scale do you want notes for?";
-    // If the user either does not reply to the welcome message or says something that is not
-    // understood, they will be prompted again with this text.
-    var repromptText = "For instructions on what you can say, please say help me.";
-    response.ask(speechText, repromptText);
-};
-
-HowTo.prototype.intentHandlers = {
-    "GetScale": function (intent, session, response) {
-        var scaleSlot = intent.slots.Scale,
-            scaleName;
-        if (scaleSlot && scaleSlot.value){
-            scaleName = scaleSlot.value.toLowerCase();
-        }
-        var patternSlot = intent.slots.Pattern,
-            patternName;
-        if (patternSlot && patternSlot.value){
-            patternName = patternSlot.value.toLowerCase();
-        }
-        var lookup = scaleName + ' ' + patternName;
-        var cardTitle = "Notes for " + lookup,
-            scale = scales[lookup],
-            speechOutput,
-            repromptOutput;
-        if (scale) {
-            speechOutput = {
-                speech: "The notes are " + scale,
-                type: AlexaSkill.speechOutputType.PLAIN_TEXT
-            };
-            response.tellWithCard(speechOutput, cardTitle, scale);
-        } else {
-            var speech = "";
-            if (scaleName && patternName){
-                speech = "I currently don't know the notes for "+ lookup+". What else can I help with?";
-            }
-            else {
-                speech = "Sorry, I didn't understand that. Please try again."
-            }
-            speechOutput = {
-                speech: speech,
-                type: AlexaSkill.speechOutputType.PLAIN_TEXT
-            };
-            repromptOutput = {
-                speech: "Name a scale",
-                type: AlexaSkill.speechOutputType.PLAIN_TEXT
-            };
-            response.ask(speechOutput, repromptOutput);
-        }
-    },
-
-    "AMAZON.StopIntent": function (intent, session, response) {
-        var speechOutput = "See ya";
-        response.tell(speechOutput);
-    },
-
-    "AMAZON.CancelIntent": function (intent, session, response) {
-        var speechOutput = "Goodbye";
-        response.tell(speechOutput);
-    },
-
-    "AMAZON.HelpIntent": function (intent, session, response) {
-        var speechText = "Supported scales are major, natural minor, melodic minor, and harmonic minor.\
-        For example, just say F melodic minor, or, you can say exit... Now, what can I help you with?";
-        var repromptText = "If you want to hear the options again, just say Help.";
-        var speechOutput = {
-            speech: speechText,
-            type: AlexaSkill.speechOutputType.PLAIN_TEXT
-        };
-        var repromptOutput = {
-            speech: repromptText,
-            type: AlexaSkill.speechOutputType.PLAIN_TEXT
-        };
-        response.ask(speechOutput, repromptOutput);
-    }
-};
-
-exports.handler = function (event, context) {
-    var howTo = new HowTo();
-    howTo.execute(event, context);
 };
